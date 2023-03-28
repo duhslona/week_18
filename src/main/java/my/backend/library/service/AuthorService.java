0351c0ba96dee3bf21f55ad795aa889c2da39870
Olga Kopina <olga.kopina@nordigy.ru>
2023-03-28 23:40:41 +0200

    Task for week 19: 1) create book requests as in lesson 2) Create Author requests

--- src/main/java/my/backend/library/service/AuthorService.java
+++ src/main/java/my/backend/library/service/AuthorService.java
@@ -2,7 +2,15 @@ package my.backend.library.service;
 
 import my.backend.library.dto.AuthorDto;
 
+import java.util.List;
+
 public interface AuthorService {
 
     AuthorDto getAuthorById(Long id);
+
+    List<AuthorDto> getAuthorByName(String name);
+
+    List<AuthorDto> getAuthorByNameV2(String name);
+
+    List<AuthorDto> getByNameV3(String name);
 }
