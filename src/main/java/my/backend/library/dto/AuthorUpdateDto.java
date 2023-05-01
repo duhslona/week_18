dfbc035e30a8218246fd6c4b3f19ffddf245e245
Olga Kopina <olga.kopina@nordigy.ru>
2023-05-01 22:56:07 +0200

    add validation

--- src/main/java/my/backend/library/dto/AuthorUpdateDto.java
+++ src/main/java/my/backend/library/dto/AuthorUpdateDto.java
@@ -1,6 +1,8 @@
 package my.backend.library.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +18,10 @@ import java.util.List;
 public class AuthorUpdateDto {
 
     private Long id;
+    @NotBlank(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 30, message = "Имя должно содержать от 2 до 30 символов")
     private String name;
+    @NotBlank(message = "Фамилия не должна быть пустой")
     private String surname;
 
     private List<BookDto> books;
