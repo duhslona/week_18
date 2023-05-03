5cec054070242679fe78e2ebb0565d8ac11c4d95
Olga Kopina <olga.kopina@nordigy.ru>
2023-05-03 23:43:37 +0200

    add tests

---
+++ src/test/java/my/backend/library/AuthorServiceTest.java
@@ -0,0 +1,58 @@
+package my.backend.library;
+
+import my.backend.library.dto.AuthorDto;
+import my.backend.library.model.Author;
+import my.backend.library.model.Book;
+import my.backend.library.repository.AuthorRepository;
+import my.backend.library.service.AuthorServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class AuthorServiceTest {
+
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @InjectMocks
+    private AuthorServiceImpl authorService;
+
+    @Test
+    public void testGetAuthorById() {
+        Long id = 1L;
+        String name = "John";
+        String surname = "Doe";
+        Set<Book> books = new HashSet<>();
+        Author author = new Author(id, name, surname, books);
+
+        when(authorRepository.findById(id)).thenReturn(Optional.of(author));
+
+        AuthorDto authorDto = authorService.getAuthorById(id);
+
+        verify(authorRepository).findById(id);
+        Assertions.assertEquals(authorDto.getId(), author.getId());
+        Assertions.assertEquals(authorDto.getName(), author.getName());
+        Assertions.assertEquals(authorDto.getSurname(), author.getSurname());
+    }
+
+    @Test
+    public void testGetAuthorByIdNotFound() {
+        Long id = 1L;
+        when(authorRepository.findById(id)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> authorService.getAuthorById(id));
+
+        verify(authorRepository).findById(id);
+    }
+}
